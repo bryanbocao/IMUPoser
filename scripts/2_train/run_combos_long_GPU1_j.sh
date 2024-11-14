e8d9dfb73920339e072690964e36042f66f12575
Bryan Bo Ca <bryan.bo.cao@outlook.com>
2024-11-13 21:20:04 -0500

    Training and testing with mpjve

---
+++ scripts/2_train/run_combos_long_GPU1_j.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+combos='global'
+
+for combo in $combos
+do 
+  echo Running combo $combo
+  CUDA_VISIBLE_DEVICES=1 python 1_train_global_model_j.py --combo_id $combo --experiment 'IMUPoserGlobalModel'
+done
--- src/imuposer/models/LSTMs/IMUPoser_Model.py
+++ src/imuposer/models/LSTMs/IMUPoser_Model.py
@@ -37,6 +37,8 @@ class IMUPoserModel(pl.LightningModule):
 
         if config.loss_type == "mse":
             self.loss = nn.MSELoss()
+        elif config.loss_type == 'mpjve':
+            self.loss = nn.PairwiseDistance(p=2)
         else:
             self.loss = nn.L1Loss()
 
@@ -65,25 +67,37 @@ class IMUPoserModel(pl.LightningModule):
         '''
         _target = target_pose
         target_pose = _target[:, :, :self.n_pose_output]
-        loss = self.loss(pred_pose, target_pose)
+        if self.config.loss_type == 'mpjve':
+            loss = 0
+        else:
+            loss = self.loss(pred_pose, target_pose)
         if self.config.use_joint_loss:
             pred_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(pred_pose).view(-1, 216))[1]
             target_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(target_pose).view(-1, 216))[1] ## If training is slow, get this from the dataloader
-            # print(f'\n IMUPoser_Model.py - training_step() - pred_joint.size(): {pred_joint.size()}')
-            # print(f'\n IMUPoser_Model.py - training_step() - target_joint.size(): {target_joint.size()}')
+            # print('\npred_joint.size(): ', pred_joint.size())
+            # print('\ntarget_joint.size(): ', target_joint.size())
             '''
-            IMUPoser_Model.py - training_step() - _pred.size(): torch.Size([256, 125, 144])
-            IMUPoser_Model.py - training_step() - pred_pose.size(): torch.Size([256, 125, 144])
-            IMUPoser_Model.py - training_step() - pred_joint.size(): torch.Size([32000, 24, 3])
-            IMUPoser_Model.py - training_step() - target_joint.size(): torch.Size([32000, 24, 3])
+            pred_joint.size():  torch.Size([8000, 24, 3])
+            target_joint.size():  torch.Size([8000, 24, 3])
             '''
-            '''
-            torch.Size([256, 125, 144]) should be (batch_size, sequence_length, 144_SMPL_pose_params)
-            125 should be sequence length. 32000 = 256*125, so make sure to reshape. 
-            is torch.Size([32000, 24, 3]) (batch_size * window_length, 24_joints, global_pose_3D_position)
-            '''
-            joint_pos_loss = self.loss(pred_joint, target_joint)
-            loss += joint_pos_loss
+            
+            if self.config.loss_type == 'mpjve':
+                # print('\npred_joint: ', pred_joint)
+                # print('\ntarget_joint: ', target_joint)
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                print('\njoint_pos_loss.size(): ', joint_pos_loss.size())
+                # joint_pos_loss.size():  torch.Size([8000, 24])
+                joint_pos_loss = torch.mean(joint_pos_loss)
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                loss += joint_pos_loss
+                
+                print('\nloss: ', loss)
+            else:
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                loss += joint_pos_loss
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                # e.g. joint_pos_loss:  tensor(0.1298, device='cuda:0')
+                print('\nloss: ', loss)
 
         self.log(f"training_step_loss", loss.item(), batch_size=self.batch_size)
         # www
@@ -97,34 +111,10 @@ class IMUPoserModel(pl.LightningModule):
         pred_pose = _pred[:, :, :self.n_pose_output]
         _target = target_pose
         target_pose = _target[:, :, :self.n_pose_output]
-        loss = self.loss(pred_pose, target_pose)
-        if self.config.use_joint_loss:
-            pred_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(pred_pose).view(-1, 216))[1]
-            target_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(target_pose).view(-1, 216))[1] ## If training is slow, get this from the dataloader
-            joint_pos_loss = self.loss(pred_joint, target_joint)
-            loss += joint_pos_loss
-
-        self.log(f"validation_step_loss", loss.item(), batch_size=self.batch_size)
-
-        return {"loss": loss}
-    
-    def test_step(self, batch, batch_idx):
-        print('\nlen(batch): ', len(batch))
-        imu_inputs, target_pose, input_lengths, _ = batch
-        print('\nimu_inputs.size(): ', imu_inputs.size())
-        print('\ntarget_pose.size(): ', target_pose.size())
-        '''
-        imu_inputs.size():  torch.Size([64, 125, 60])
-        target_pose.size():  torch.Size([64, 125, 144])
-        '''
-        print('\ninput_lengths: ', input_lengths)
-
-        _pred = self(imu_inputs, input_lengths)
-
-        pred_pose = _pred[:, :, :self.n_pose_output]
-        _target = target_pose
-        target_pose = _target[:, :, :self.n_pose_output]
-        loss = self.loss(pred_pose, target_pose)
+        if self.config.loss_type == 'mpjve':
+            loss = 0
+        else:
+            loss = self.loss(pred_pose, target_pose)
         if self.config.use_joint_loss:
             pred_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(pred_pose).view(-1, 216))[1]
             target_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(target_pose).view(-1, 216))[1] ## If training is slow, get this from the dataloader
@@ -134,12 +124,85 @@ class IMUPoserModel(pl.LightningModule):
             pred_joint.size():  torch.Size([8000, 24, 3])
             target_joint.size():  torch.Size([8000, 24, 3])
             '''
-            joint_pos_loss = self.loss(pred_joint, target_joint)
-            loss += joint_pos_loss
+            
+            if self.config.loss_type == 'mpjve':
+                # print('\npred_joint: ', pred_joint)
+                # print('\ntarget_joint: ', target_joint)
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                print('\njoint_pos_loss.size(): ', joint_pos_loss.size())
+                # joint_pos_loss.size():  torch.Size([8000, 24])
+                joint_pos_loss = torch.mean(joint_pos_loss)
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                loss += joint_pos_loss
+                
+                print('\nloss: ', loss)
+            else:
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                loss += joint_pos_loss
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                # e.g. joint_pos_loss:  tensor(0.1298, device='cuda:0')
+                print('\nloss: ', loss)
 
-        self.log(f"validation_step_loss", loss.item(), ', batch_size: ', self.test_batch_size)
-        print(f"validation_step_loss", loss.item(), ', batch_size: ', self.test_batch_size)
+        self.log(f"validation_step_loss", loss.item(), batch_size=self.batch_size)
+
+        return {"loss": loss}
+    
+    def test_step(self, batch, batch_idx):
+        # print('\nlen(batch): ', len(batch))
+        imu_inputs, target_pose, input_lengths, _ = batch
+        # print('\nimu_inputs.size(): ', imu_inputs.size())
+        # print('\ntarget_pose.size(): ', target_pose.size())
+        '''
+        imu_inputs.size():  torch.Size([64, 125, 60])
+        target_pose.size():  torch.Size([64, 125, 144])
+        '''
+        # print('\ninput_lengths: ', input_lengths)
+        '''
+        input_lengths:  [125, 125, 125, 125, 125, 11, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 11, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 11, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 8, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 8, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 8]
+        '''
+
+        _pred = self(imu_inputs, input_lengths)
+
+        pred_pose = _pred[:, :, :self.n_pose_output]
+        _target = target_pose
+        target_pose = _target[:, :, :self.n_pose_output]
+        
+        if self.config.loss_type == 'mpjve':
+            loss = 0
+        else:
+            loss = self.loss(pred_pose, target_pose)
+        if self.config.use_joint_loss:
+            pred_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(pred_pose).view(-1, 216))[1]
+            target_joint = self.bodymodel.forward_kinematics(pose=r6d_to_rotation_matrix(target_pose).view(-1, 216))[1] ## If training is slow, get this from the dataloader
+            # print('\npred_joint.size(): ', pred_joint.size())
+            # print('\ntarget_joint.size(): ', target_joint.size())
+            '''
+            pred_joint.size():  torch.Size([8000, 24, 3])
+            target_joint.size():  torch.Size([8000, 24, 3])
+            '''
+            
+            if self.config.loss_type == 'mpjve':
+                # print('\npred_joint: ', pred_joint)
+                # print('\ntarget_joint: ', target_joint)
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                print('\njoint_pos_loss.size(): ', joint_pos_loss.size())
+                # joint_pos_loss.size():  torch.Size([8000, 24])
+                joint_pos_loss = torch.mean(joint_pos_loss)
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                loss += joint_pos_loss
+                
+                print('\nloss: ', loss)
+            else:
+                joint_pos_loss = self.loss(pred_joint, target_joint)
+                loss += joint_pos_loss
+                print('\njoint_pos_loss: ', joint_pos_loss)
+                # e.g. joint_pos_loss:  tensor(0.1298, device='cuda:0')
+                print('\nloss: ', loss)
+
+        self.log(f"test_step_loss", loss.item(), ', batch_size: ', self.test_batch_size)
+        print(f"test_step_loss", loss.item(), ', batch_size: ', self.test_batch_size)
         print('\npred_joint.size(): ', pred_joint.size())
+        # in meters
         return {"loss": loss}
 
     def predict_step(self, batch, batch_idx):
